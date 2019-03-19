1ecc16decf321c8413bcf09784a64c72f7f0563a
kevin <kjwilde@hotmail.com>
2019-03-18 22:53:22 -0500

    Fixing error in bitcoin.conf that may be preventing it from restarting properly.

--- docker/start-bitcoind.sh
+++ docker/start-bitcoind.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-echo "\n" >> /bitcoin/.bitcoin/bitcoin.conf
+#if [ ! -f "/tmp/bitcoin.conf" ];
+#then
+#    cp /bitcoin/.bitcoin/bitcoin.conf /tmp/
+#fi
+#
+#cp /tmp/bitcoin.conf /bitcoin/.bitcoin/bitcoin.conf
 
-/tmp/setup-rpc.sh | grep rpcauth >> /bitcoin/.bitcoin/bitcoin.conf
+rpcAuthLineNumber=$(grep -n rpcauth /bitcoin/.bitcoin/bitcoin.conf | head -n 1 | cut -d: -f1)
+rpcAuth=$(/tmp/setup-rpc.sh | grep rpcauth)
+#rpcAuth=$(echo "rpcauth=blah")
+
+if [[ ${rpcAuthLineNumber} != "" ]]
+then
+    sed -i -e "${rpcAuthLineNumber}s/.*/${rpcAuth}/" /bitcoin/.bitcoin/bitcoin.conf
+
+else
+    echo "" >> /bitcoin/.bitcoin/bitcoin.conf
+    echo ${rpcAuth} >> /bitcoin/.bitcoin/bitcoin.conf
+fi
 
 docker-entrypoint.sh btc_oneshot
 
